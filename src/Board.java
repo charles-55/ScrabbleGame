59ebf6c31fd0bca11571797226020a4f82874523
Osamudiamen Nwoko <carlosnwoko123@gmail.com>
2022-10-18 22:55:25 -0400

    Added unimplemented methods to Board and GameMaster

--- src/Board.java
+++ src/Board.java
@@ -1,9 +1,29 @@
 public class Board {
 
+    public enum Direction {FORWARD, DOWNWARD}
+
     private Square[][] board;
     private final int BOARD_SIZE = 15;
 
     public Board() {
         board = new Square[BOARD_SIZE][BOARD_SIZE];
+        this.initializeBoard();
+    }
+
+    private void initializeBoard() {
+        for(Square[] squares : board) {
+            for(Square square : squares) {
+                square = new Square();
+            }
+        }
+    }
+
+    public boolean attemptPlay(Tile[] word, int[] coordinates, Direction direction) {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "";
     }
 }
--- src/GameMaster.java
+++ src/GameMaster.java
@@ -3,4 +3,33 @@ public class GameMaster {
     private Board board;
     private Bag bag;
     private Player[] players;
+    private int turn;
+    private Parser parser;
+
+    public GameMaster() {
+        board = new Board();
+        bag = new Bag();
+        turn = 0;
+        parser = new Parser();
+    }
+
+    private void initializePlayers(int numPlayers) {
+        players = new Player[numPlayers];
+
+        for(Player player : players) {
+            player = new Player();  // needs to initialize each player
+        }
+    }
+
+    public boolean processCommand(Command command) {
+        return false;
+    }
+
+    public boolean attemptPlay(String word) {
+        return false;
+    }
+
+    public void playGame() {
+
+    }
 }

8603ae05099c7eed2f1994d54bfe37ca483ae756
Osamudiamen Nwoko <carlosnwoko123@gmail.com>
2022-10-19 01:26:01 -0400

    Updated Board and GameMaster classes; still have incomplete implementations

--- src/Board.java
+++ src/Board.java
@@ -5,11 +5,17 @@ public class Board {
     private Square[][] board;
     private final int BOARD_SIZE = 15;
 
+    /**
+     * Create the board.
+     */
     public Board() {
         board = new Square[BOARD_SIZE][BOARD_SIZE];
         this.initializeBoard();
     }
 
+    /**
+     * Initialize the board.
+     */
     private void initializeBoard() {
         for(Square[] squares : board) {
             for(Square square : squares) {
@@ -18,12 +24,59 @@ public class Board {
         }
     }
 
+    // TODO: 2022-10-19 complete implementation
+    /**
+     * Attempts to play a particular word on the board.
+     * @param word An array of tiles that spells the word.
+     * @param coordinates The coordinate of the starting
+     *                    point of the word on the board.
+     * @param direction The direction in which the word is
+     *                  spelt on the board.
+     * @return true if the attempt was successful, false
+     * otherwise.
+     */
     public boolean attemptPlay(Tile[] word, int[] coordinates, Direction direction) {
         return false;
     }
 
+    /**
+     * Return a string representation of the board.
+     * @return A string representation of the board.
+     */
     @Override
     public String toString() {
-        return "";
+        String boardString = "  || 0";
+
+        for(int i = 1; i < BOARD_SIZE; i++) {
+            if(i <= 10)
+                boardString += " | " + i;
+            else
+                boardString += "| " + i;
+        }
+        boardString += """
+
+                ---------------------------------------------------------------
+                """;
+
+        int verticalCoordinate = 0;
+        for(Square[] squares : board) {
+            if(verticalCoordinate < 10)
+                boardString += " " + verticalCoordinate + "|";
+            else
+                boardString += verticalCoordinate + "|";
+
+            for(Square square : squares) {
+                Tile tile = square.getTile();
+                if(tile == null)
+                    boardString += " |   ";
+                else
+                    boardString += " | " + tile.getLetter() + " ";
+            }
+            boardString += "\n";
+
+            verticalCoordinate++;
+        }
+
+        return boardString;
     }
 }
--- src/GameMaster.java
+++ src/GameMaster.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class GameMaster {
 
     private Board board;
@@ -6,6 +10,9 @@ public class GameMaster {
     private int turn;
     private Parser parser;
 
+    /**
+     * Create and initialize the game.
+     */
     public GameMaster() {
         board = new Board();
         bag = new Bag();
@@ -13,23 +20,172 @@ public class GameMaster {
         parser = new Parser();
     }
 
+    /**
+     * Return the opening message for the player.
+     * @return The opening message.
+     */
+    private String getWelcomeMessage() {
+        return """
+                Welcome to the game of Scrabble!
+                Scrabble is a board spelling game.
+                Type 'help' if you need help.
+                How many players would be playing today?
+                """;
+    }
+
+    /**
+     * Initialize all the players.
+     * @param numPlayers The number of players playing the game.
+     */
     private void initializePlayers(int numPlayers) {
         players = new Player[numPlayers];
 
-        for(Player player : players) {
-            player = new Player();  // needs to initialize each player
+        for(int i = 0; i < players.length; i++) {
+            String playerName = parser.getPlayerName(i);
+            players[i] = new Player(playerName);  // needs to initialize each player
         }
     }
 
+    /**
+     * Given a command, process (that is: execute) the command
+     * @param command The command to be processed.
+     * @return true if the command ends the game, false otherwise.
+     */
     public boolean processCommand(Command command) {
+        boolean wantToQuit = false;
+
+        if(command.isUnknown()) {
+            System.out.println("I don't know what you mean...");
+            return false;
+        }
+
+        String commandWord = command.getCommandWord();
+        if (commandWord.equals("help")) {
+            System.out.println(printHelp());
+        }
+        else if (commandWord.equals("play")) {
+            attemptPlay(command);
+        }
+        else if (commandWord.equals("quit")) {
+            wantToQuit = quit(command);
+        }
+        else if (commandWord.equals("save")) {
+            save(command);
+        }
+        else if (commandWord.equals("load")) {
+            load(command);
+        }
+
+        return wantToQuit;
+    }
+
+    // TODO: 2022-10-18 complete implementation
+    /**
+     * Return a message of help information and
+     * a list of command words.
+     * @return A message of help information and
+     * a list of command words.
+     */
+    private String printHelp() {
+        return """
+                You are supposed to 
+                
+                Your command words are:
+                help, play, quit, save, load
+                """;
+    }
+
+    // TODO: 2022-10-18 complete implementation
+    /**
+     * Attempts to play a particular word on the board.
+     * @param command Command containing the word to play.
+     * @return true if the word is playable on the board,
+     * false otherwise.
+     */
+    public boolean attemptPlay(Command command) {
+        if(!command.hasSecondWord()) {
+            System.out.println("Play what?");
+            return false;
+        }
+
+        String wordAttempt = command.getSecondWord();
+
+        try {
+            Scanner dictionary = new Scanner(new File("WordList.txt"));
+            while(dictionary.hasNextLine()) {
+                if(wordAttempt.equals(dictionary.nextLine())) {
+                    this.board.attemptPlay();
+                }
+            }
+        }
+        catch (FileNotFoundException exception) {
+            System.out.println("Dictionary not found");
+            return false;
+        }
+
+        if (nextRoom == null) {
+            System.out.println("There is no door!");
+        }
+        else {
+            history.push(currentRoom);
+            currentRoom = nextRoom;
+            this.printDescription();
+        }
+
         return false;
     }
 
-    public boolean attemptPlay(String word) {
-        return false;
+    /**
+     * Changes the players' turn.
+     */
+    public void changeTurn() {
+        this.turn = (this.turn + 1) % players.length;
     }
 
+    // TODO: 2022-10-18 complete implementation
+    /**
+     * Save the current game being played.
+     * @param command Command to save the game
+     */
+    private void save(Command command) {
+    }
+
+    // TODO: 2022-10-18 complete implementation
+    /**
+     * Load a saved game.
+     * @param command Command to load the saved game
+     */
+    private void load(Command command) {
+    }
+
+    /**
+     * Quits the game.
+     * @param command command to quit the game.
+     * @return true if command quits the game,
+     * false otherwise.
+     */
+    private boolean quit(Command command) {
+        if(command.hasSecondWord()) {
+            System.out.println("Quit what?");
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    /**
+     * Main play routine.  Loops until end of play.
+     */
     public void playGame() {
+        System.out.println(this.getWelcomeMessage());
+        int numPlayers = parser.getNumPlayers();
+        this.initializePlayers(numPlayers);
 
+        boolean finished = false;
+        while(!finished) {
+            Command command = parser.getCommand();
+            finished = this.processCommand(command);
+        }
     }
 }

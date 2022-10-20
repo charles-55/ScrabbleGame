9de0e977ba0dfcd5e3cac5ed40ba9210356becb1
leslie <leslieejeh@cmail.com>
2022-10-20 19:56:19 -0400

    made updates

--- src/Player.java
+++ src/Player.java
@@ -3,11 +3,27 @@ import java.util.*;
 public class Player {
 
     private String name;
-    private int score;
+    private int score=0;
     private Rack rack;
     private Stack playedWords;
 
-    public Player() {
+    public Player(String name) {
+        this.name = name;
+        this.rack = new Rack();
+        this.playedWords = new Stack();
+    }
+    public String getName(){
+        return this.name;
+    }
+    public void updateScore(int updateScore){
+        this.score+=updateScore;
+    }
+    public int getScore(){
+        return this.score;
+    }
+    public Rack getRack(){
+        for(int i=7; i<=7;i++){
 
+        }
     }
 }
--- src/Rack.java
+++ src/Rack.java
@@ -3,7 +3,34 @@ public class Rack {
     private Tile[] tiles;
     private final int MAX_RACK_SIZE = 7;
 
+
     public Rack() {
+        this.tiles = new Tile[MAX_RACK_SIZE];
+    }
+    public void fillRack(){
+        for(int i=0; i<7;i++){
+            tiles[i]=new Tile();
+        }
+    }
+    public void exchangeTiles(int[] tilesToExchangeIndex){
+        for(int i=0;i<7;i++){
+
+        }
+
+
 
     }
+    public Tile[] getTiles(){
+        return tiles;
+    }
+    public String toString(){
+        String characterAndPoints;
+        for(Tile t:tiles){
+            characterAndPoints="letter="+" "+t.getLetter()+" "+ t.getPoints()+"\n" ;
+            return characterAndPoints;
+        }
+        return "this is all tiles";
+
+    }
+
 }

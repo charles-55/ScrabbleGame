7d298d99bee89b12497cfbbe6dccecc014965c49
Osamudiamen Nwoko <carlosnwoko123@gmail.com>
2022-10-22 17:11:51 -0400

    Updated Main for testing

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        //new GameMaster();
-        System.out.println((new Board()).toString());
+        new GameMaster();
     }
 }

56c9dd5ef94cd57822194f4f19e798017a035718
leslie <leslieejeh@cmail.com>
2022-10-24 20:37:26 -0400

    added documentations to player and rack

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,9 @@
-
+/**
+ * The Player class.
+ *
+ * @author Ejeh Leslie 101161386
+ * @version 1.0
+ */
 import java.util.*;
 public class Player {
 
@@ -7,23 +12,49 @@ public class Player {
     private Rack rack;
     private Stack playedWords;
 
+    /**
+     * Create and initialize the Player.
+     */
     public Player(String name) {
         this.name = name;
         this.rack = new Rack();
         this.playedWords = new Stack();
     }
+
+    /**
+     * gets the players name
+     * @return string  0f the players name.
+     */
     public String getName(){
         return this.name;
     }
+
+    /**
+     * Updates the players score
+     */
     public void updateScore(int updateScore){
         this.score+=updateScore;
     }
+
+    /**
+     * Gets the player score
+     * @return data type of int corresponding to a  player score
+     */
     public int getScore(){
         return this.score;
     }
+    /**
+     * Gets the players rack
+     * @return Data of type rack
+     */
     public Rack getRack(){
         return rack;
     }
+
+    /**
+     * Gets the players name, score and rack in a string
+     * @return a string
+     */
     public String toString(){
         String s = "name = " + getName()+" Score = "+getScore()+ "\n rack = "+rack.toString();
         return s;
--- src/Rack.java
+++ src/Rack.java
@@ -1,12 +1,27 @@
+/**
+ * The Rack class.
+ *
+ * @author Ejeh Leslie 101161386
+ * @version 1.0
+ */
 public class Rack {
 
     private Tile[] tiles;
     private final int MAX_RACK_SIZE = 7;
 
+    /**
+     * Create and initialize the Rack.
+     */
 
     public Rack() {
         this.tiles = new Tile[MAX_RACK_SIZE];
     }
+
+    /**
+     * takes a tile from the bag and fills the rack up
+     * @param bag The Bag to be processed.
+     * @return true if the bag was filled , false otherwise.
+     */
     public boolean fillRack(Bag bag){
         boolean filled=true;
         for(int i=0;i<MAX_RACK_SIZE;i++){
@@ -18,6 +33,11 @@ public class Rack {
         }
         return filled;
     }
+    /**
+     * checks the bag and true  if the amount of tile to be exhanged was succesful
+     * @param bag and an array of int.
+     * @return true if the tile was exchanged succesfully , false otherwise.
+     */
     public boolean exchangeTiles(Bag bag,int[] index){
         for(int i : index) {
             if(!bag.isEmpty()) {
@@ -26,9 +46,18 @@ public class Rack {
         }
         return true;
     }
+    /**
+     * returns the array of  tile on the rack
+     * @return an array of tile
+     */
     public Tile[] getTiles(){
         return tiles;
     }
+
+    /**
+     * gets the tiles character and string
+     * @return String  of the tiles character and points
+     */
     public String toString(){
         String characterAndPoints="| ";
         for(Tile t:tiles){
@@ -38,6 +67,11 @@ public class Rack {
         }
         return characterAndPoints;
     }
+
+    /**
+     * removes a tile from the rack
+     * @param tile to remove a tile
+     */
 
     public void removeTile(Tile tile) {
         for(Tile t : tiles) {
